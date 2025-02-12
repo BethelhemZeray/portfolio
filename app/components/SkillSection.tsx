2e60b8fa8910cc10ffd23dc314a73f5a61c84cca
AbeDevSaga <2000amandanm@gmail.com>
2025-02-12 17:52:34 +0300

    skill headers

--- app/components/SkillSection.tsx
+++ app/components/SkillSection.tsx
@@ -8,12 +8,18 @@ type SkillSectionProps = {
 
 function SkillSection({ skills }: SkillSectionProps) {
   return (
-    <div className="flex flex-col md:p-10 lg:flex-row items-start justify-center lg:justify-between lg:py-4 lg:gap-14 gap-4 md:gap-8">
-      <p className="text-primary text-lg px-4">Skills</p>
-      {skills.map((skill, index) => (
-        <ProgressChart key={index} title={skill.title} skills={skill.skills} />
-      ))}
-    </div>
+    <>
+      <p className="text-primary text-lg px-4 lg:px-14">Skills</p>
+      <div className="flex flex-col md:p-10 lg:flex-row items-start justify-center lg:justify-between lg:py-4 lg:gap-14 gap-4 md:gap-8">
+        {skills.map((skill, index) => (
+          <ProgressChart
+            key={index}
+            title={skill.title}
+            skills={skill.skills}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
