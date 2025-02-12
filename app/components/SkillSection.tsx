adc2788dff3386c40a95784823bc5bb76cca1f08
AbeDevSaga <2000amandanm@gmail.com>
2025-02-12 17:54:17 +0300

    modified

--- app/components/SkillSection.tsx
+++ app/components/SkillSection.tsx
@@ -9,7 +9,7 @@ type SkillSectionProps = {
 function SkillSection({ skills }: SkillSectionProps) {
   return (
     <>
-      <p className="text-primary text-lg px-4 lg:px-14">Skillsin eng</p>
+      <p className="text-primary text-lg px-4 lg:px-14">Skill</p>
       <div className="flex flex-col md:p-10 lg:flex-row items-start justify-center lg:justify-between lg:py-4 lg:gap-14 gap-4 md:gap-8">
         {skills.map((skill, index) => (
           <ProgressChart
