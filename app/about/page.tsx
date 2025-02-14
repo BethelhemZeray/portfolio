71df3334dfc9bd0a37ae425669dcd4d01441c56c
AbeDevSaga <2000amandanm@gmail.com>
2025-02-14 16:38:09 +0300

    complete change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import CountSection from "../components/CountSection";
+import DoubleGraduationSection from "../components/DoubleGraduationSection";
 import ExperianceCard from "../components/ExperianceCard";
-import GraduationSection from "../components/GraduationSection";
 import SectionHeader from "../components/SectionHeader";
 
 
@@ -8,7 +8,7 @@ const AboutSection = () => {
   return (
     <section id="about" className="bg-background px-4">
       <SectionHeader sectionKey="about"/>
-      <GraduationSection/>
+      <DoubleGraduationSection/>
       <div className="flex flex-col lg:flex-row items-top justify-center lg:justify-between h-full space-y-10 lg:space-y-0 py-4">
         <div className="lg:w-1/2 text-left lg:p-6 mt-8 lg:mt-0 rounded-lg backdrop-blur-sm">
           <ExperianceCard />
--- app/constants/graduation.ts
+++ app/constants/graduation.ts
@@ -1,5 +1,6 @@
 import { TGraduation } from "./type";
 import haramaya from '../../public/images/haramaya.png';
+import AddisAbaba from '../../public/images/AddisAbaba.png';
 
 
 export const graduation: TGraduation[] = [
@@ -7,7 +8,15 @@ export const graduation: TGraduation[] = [
         universityLogo: haramaya, 
         year: "2016", 
         name: "Haramaya University", 
-        dept: "Civil Engineering",
+        dept: "Bsc. In Civil Engineering",
+        gpa: "3.8", 
+        resume: "/resume.pdf",
+      },
+      {
+        universityLogo: AddisAbaba, 
+        year: "2025", 
+        name: "Addis Ababa University", 
+        dept: "Msc. In Construction Technology And Management",
         gpa: "3.8", 
         resume: "/resume.pdf",
       },
