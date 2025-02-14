9b55d6dcb5dd0cae7c80709faf3703e7652a681a
AbeDevSaga <2000amandanm@gmail.com>
2025-02-14 16:56:27 +0300

    new change

--- app/components/DoubleGraduationSection.tsx
+++ app/components/DoubleGraduationSection.tsx
@@ -5,7 +5,7 @@ import GraduationCard from "./GraduationCard";
 
 const DoubleGraduationSection = () => {
   return (
-    <div className="grid grid-col-1 lg:grid-cols-2 gap-4 p-2 rounded-lg shadow-md">
+    <div className="grid grid-col-1 lg:grid-cols-2 gap-4 lg:gap-6 p-2 rounded-lg shadow-md">
       {graduation.map((data, index) => (
         <GraduationCard key={index} data={data} />
       ))}
